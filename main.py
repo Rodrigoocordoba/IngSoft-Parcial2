c7ec271981866a62660f4a53b602e55241535e74
Rodrigoocordoba <rodrigoojcordoba@gmail.com>
2024-06-10 22:02:01 -0300

    codigo que no debe dar error

--- main.py
+++ main.py
@@ -2,27 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-var1 = "Hello World"
-var2 = "Hello World"  # Variable duplicada para simular código duplicado
+var1="Hello World"
 
 @app.get("/")
 async def root():
     return {"message": var1}
-
-@app.get("/duplicate")
-async def duplicate():
-    return {"message": var2}  # Código duplicado
-
-# Código comentado innecesario
-# def unused_function():
-#     pass
-
-# Nombre de variable poco descriptivo
-a = "Another Hello World"
-b = "Another Hello World"  # Código duplicado y nombre de variable poco descriptivo
-
-def some_function():
-    return a
-
-def another_function():
-    return b  # Código duplicado y nombre de variable poco descriptivo
