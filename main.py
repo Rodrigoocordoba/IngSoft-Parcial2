76b4c415fdddc8b0a6ed3d37ad5fd072fdd303ad
Rodrigoocordoba <rodrigoojcordoba@gmail.com>
2024-06-13 17:38:07 -0300

    codigo que funciona

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ app = FastAPI()
 
 var1="Hello World"
 
-465475464476
-
 @app.get("/")
 async def root():
-    if True:
-        return {"message": var1}
-    else:
-        return {"message": var1}
+    return {"message": var1}
